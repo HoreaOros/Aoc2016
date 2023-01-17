12abc3ac08c931b494b8fbf972466d8646c1f2eb
Horea Oros <horea.oros@gmail.com>
2023-01-17 18:37:00 +0200

    add day 14 solution

--- 14.py
+++ 14.py
@@ -2,45 +2,71 @@ import hashlib
 from collections import deque
 import re
 salt = 'yjdafjpo'
+
 keys = 64
-index = 0
-hashlist = deque()
+def getRepeatedHash(str):
+    hash = hashlib.md5(str.encode()).hexdigest()
+    for _ in range(2016):
+        hash = hashlib.md5(hash.encode()).hexdigest()
+    return hash
 
-#result = hashlib.md5(str2hash.encode()).hexdigest()
+def part1():
+    index = 0
+    count  = 0
+    while True:
+        while True:
+            hash1 = hashlib.md5((salt + str(index)).encode()).hexdigest()
 
-keyCount = 0
+            m = re.search(r'(.)\1{2}', hash1) 
+            if  m != None:
+                #print(hash1, index)
+                break
+            index += 1
 
-
-
-# gasesc primul hash cu 3 caractere repetate
-while True:
-    hash = hashlib.md5((salt + str(index)).encode()).hexdigest()
-    m = re.search(r'(\d)\1{2}', hash) 
-    if  m != None:
         (start, _) = m.span()
-        c = hash[start]
-        break
-    index += 1
-
-# trebuie sa am 1000 de hash-uri in lista
-
-
-while keyCount < keys:
-    # adaug in hashlist  hashuri pana cand am 1000
-    n = len(hashlist)
-    if n < 1000:
-        for i in range(1000 - len(hashlist)):
-            hashlist.append(hashlib.md5((salt + str(index + 1 + i + n)).encode()).hexdigest())  
-    # caut primul hash cu 5x 
-    for item in hashlist:
-        m = re.search('(' + c + ')\1{4}', hash) 
-        if m != None:
-            keyCount += 1
+        c = hash1[start]
+        for i in range(1, 1001):
+            hash2 = hashlib.md5((salt + str(index + i)).encode()).hexdigest()
+            rx = c*5
+            m2 = re.search(rx, hash2) 
+            if m2 != None:
+                count += 1
+                print(count, hash1, index)
+                print('   ', hash2, index + i)
+                break
+        if count == keys:
             break
-    hashlist.popleft()
-    
+        index += 1
+    print(index)
 
-    
-            
-    
-    
+
+
+def part2():
+    print('Computing repeated hashes...')
+    hashes = []
+    N = 25000
+    for i in range(N):
+        hashes.append(getRepeatedHash(salt + str(i)))
+    print('Done computing repeated hashes.')
+    count = 0
+    for index in range(N):
+        m = re.search(r'(.)\1{2}', hashes[index]) 
+        if  m != None:
+            (start, _) = m.span()
+            c = hashes[index][start]
+            rx = c*5
+            for i in range(1, 1001):
+                m2 = re.search(rx, hashes[index + i]) 
+                if m2 != None:
+                    count += 1
+                    print('Found key ' + str(count) + ' at index ' + str(index))
+                    break
+        if count == 64:
+            print(index)
+            break
+
+#part1()
+
+part2()
+
+
